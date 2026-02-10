ef6bf69ec26070adfd2090c899b356d80944bb89
Kamil Barański <kamilbaranski@gmail.com>
2026-02-10 19:49:02 +0100

    more sensors, less valueType, getSensor* fwd defs

--- sensors_types.h
+++ sensors_types.h
@@ -6,25 +6,32 @@ enum SensorType : uint8_t {
   SENSOR_HUMIDITY = 2,
   SENSOR_ABSOLUTE_HUMIDITY = 3,
   SENSOR_PRESSURE = 4,
-  SENSOR_AIR_QUALITY = 5
+  SENSOR_AIR_QUALITY = 5,
+  SENSOR_CO2_PPM = 6,
+  SENSOR_VOC_PPB = 7
 };
 
 const char *sensorTypeStrs[] = {
-    "unknown",         // SENSOR_UNKNOWN
-    "temperature",     // SENSOR_TEMP
-    "humidity",        // SENSOR_HUMIDITY
-    "absoluteHumidity" // SENSOR_ABSOLUTE_HUMIDITY
-                       //    "pressure",      // SENSOR_PRESSURE
-                       //    "air_quality"    // SENSOR_AIR_QUALITY
+    "unknown",                   // SENSOR_UNKNOWN
+    "temperature",               // SENSOR_TEMP
+    "humidity",                  // SENSOR_HUMIDITY
+    "absolute_humidity",         // SENSOR_ABSOLUTE_HUMIDITY
+    "pressure",                  // SENSOR_PRESSURE
+    "air_quality",               // SENSOR_AIR_QUALITY
+    "carbon_dioxide",            // SENSOR_CO2_PPM
+    "volatile_organic_compounds_parts" // SENSOR_VOC_PPB
 };
 
 const char *sensorUnits[] = {
-    "",     // SENSOR_UNKNOWN
-    " °C",  // SENSOR_TEMP
-    "%",    // SENSOR_HUMIDITY
-    " g/m³" // SENSORS_ABSOLUTE_HUMIDITY
-            //    "hPa",      // SENSOR_PRESSURE
-            //    "µg/m³"    // SENSOR_AIR_QUALITY
+    "",      // SENSOR_UNKNOWN
+    " °C",   // SENSOR_TEMP
+    "%",     // SENSOR_HUMIDITY
+    " g/m³", // SENSORS_ABSOLUTE_HUMIDITY
+    " hPa",   // SENSOR_PRESSURE
+    " µg/m³", // SENSOR_AIR_QUALITY
+    " ppm",   // SENSOR_CO2_PPM
+    " ppb"    // SENSOR_VOC_PPB
+
 };
 
 /*
@@ -35,13 +42,13 @@ const char *sensorUnits[] = {
 struct SensorConfig {
   char address[17];         // 16 hex + \0
   char name[32];            // friendly name
+  SensorType type;          // typ sensora
   float compensation;       // temperature reading correction
   bool present;             // runtime only
   float lastValue;          // last reading
   float lastPublishedValue; // runtime only; last mqtt published value
   uint32_t lastUpdate;      // timestamp of last reading
-  char valueType[6];        // "%" or " °C" or " g/m3" + \0
-  SensorType type;          // typ sensora
+  // char valueType[6];     // "%" or " °C" or " g/m3" + \0
 };
 
 uint16_t sensorsCount = 0;
@@ -71,3 +78,7 @@ DHTStable DHT;
 OneWire oneWire(DS18B20_PIN);
 DallasTemperature sensors(&oneWire);
 DeviceAddress Thermometer;
+
+// forward definitions:
+String getSensorType(const SensorConfig &s);
+String getSensorUnits(const SensorConfig &s);
